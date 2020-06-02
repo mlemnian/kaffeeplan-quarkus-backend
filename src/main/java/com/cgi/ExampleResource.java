26dddbd0e82cc8b51dd4393266b5842ba51504ec
martin lemnian <martin.lemnian@gmail.com>
2020-06-03 00:22:29 +0200

    the resource needs to be transactional

--- src/main/java/com/cgi/ExampleResource.java
+++ src/main/java/com/cgi/ExampleResource.java
@@ -1,5 +1,7 @@
 package com.cgi;
 
+import javax.enterprise.context.RequestScoped;
+import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,13 +9,15 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/weeks")
+@RequestScoped
 public class ExampleResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
     public List<KaffeeplanEntry> hello() {
-        KaffeeplanEntry kaffeeplanEntry = new KaffeeplanEntry("202023","test","test@test.de");
-        kaffeeplanEntry.persist();
+        KaffeeplanEntry kaffeeplanEntry = new KaffeeplanEntry(null,"test","test@test.de");
+        kaffeeplanEntry.persistAndFlush();
         return KaffeeplanEntry.listAll();
     }
 }
